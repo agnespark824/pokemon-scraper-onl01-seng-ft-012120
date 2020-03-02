705eae31963f08743fbb13fbf036b1224bb57d75
Agnes Park <agnespark824@gmail.com>
2020-03-02 06:07:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,8 +16,8 @@ class Pokemon
         INSERT INTO pokemon (name, type)
         VALUES (?, ?)
         SQL
-    @db.execute(sql, name, type)
-    @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
   def self.find(id)
